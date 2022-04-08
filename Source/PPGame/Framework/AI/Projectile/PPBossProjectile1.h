acea9b3d6512c3e2a2a574af7fbf5f295fe89758
pupil1337 <2653414616@qq.com>
2022-04-08 16:32:59 +0800

    增加boss导弹类

---
+++ Source/PPGame/Framework/AI/Projectile/PPBossProjectile1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PPGame/Framework/Projectile/PPProjectileBase.h"
+#include "PPBossProjectile1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PPGAME_API APPBossProjectile1 : public APPProjectileBase
+{
+	GENERATED_BODY()
+	
+};
